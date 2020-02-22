02d81155092127bc7812238c728f0d8f339b8a1b
Guilherme <gui-kalinauskas@hotmail.com>
2020-02-21 22:30:00 -0300

    feito um peqieno programa.

---
+++ Meuprojeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Meuprojeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao pragrama no Git!");
+            Console.WriteLine("Aproveite!!!");
+        }
+    }
+}
